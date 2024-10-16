4f19876db3bf975b720755c963f3fd9c54dcd9a4
Nomindaa <nbaterdene65@gmail.com>
2024-10-16 15:13:07 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Signin, Signup, Home, Profile } from "./pages";
+import { Signin, Signup, Home, Profile, Joke, Posts } from "./pages";
 
 export const App = () => {
   return (
@@ -10,6 +10,8 @@ export const App = () => {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/joke" element={<Joke />} />
+        <Route path="/posts" element={<Posts />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,6 @@ export * from "./sign-in";
 export * from "./sign-up";
 export * from "./home";
 export * from "./profile";
+export * from "./joke";
+export * from "./posts";
+export * from "./data";
