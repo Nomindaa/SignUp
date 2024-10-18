5afaba03acfcaff27e20adf64da72b3cc040e99e
Nomindaa <nbaterdene65@gmail.com>
2024-10-17 22:45:16 -0500

    Initial commit

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -1,1 +1,26 @@
 export * from "./Signin";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Count : {this.state.count}</p>
+        <button onClick={() => this.increment}>Increase</button>
+        <button onClick={() => this.decrement}>Decrease</button>
+      </div>
+    );
+  }
+}
